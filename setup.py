11a0a4512ed96413893e931b38b6a70b9bd861a5
Lauren Lui <lauren.lui@gmail.com>
2016-10-25 00:27:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("c_antisense",sources=["c_antisense.pyx","antisense.c","utils.c"],extra_compile_args=["-std=c99"])]
+ext_modules = [Extension("c_antisense",sources=["c_antisense.pyx","antisense.c","utils.c"],extra_compile_args=["-std=c99","-Qunused-arguments","-Wno-error=unused-command-line-argument-hard-error-in-future"])]
 
 setup(
   name = 'Antisense C Module',
     cmdclass = {'build_ext': build_ext},
       ext_modules = ext_modules
       )
-
